9730f52ea1a1f38d34d01c5813df5556d026e455
Ever Solis <eversolis@theblackbit.io>
2023-09-09 16:00:12 -0600

    Jacoco configured

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("theblackbit.rickandmorty.application.compose")
     id("theblackbit.rickandmorty.hilt")
     id("org.jlleitschuh.gradle.ktlint")
+    id("theblackbit.rickandmorty.jacoco")
 }
 
 android {
