907cb35fe1243f42f635bec5e36876e26e63e6d7
Ever Solis <eversolis@theblackbit.io>
2023-09-10 03:00:58 -0600

    Networking configured with apollo client

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -4,8 +4,24 @@ plugins {
     id("theblackbit.rickandmorty.hilt")
     id("org.jlleitschuh.gradle.ktlint")
     id("theblackbit.rickandmorty.jacoco")
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
+    id("com.apollographql.apollo3") version (libs.versions.apollo)
+}
+
+apollo {
+    service("service") {
+        packageName.set("com.theblackbit.rickandmorty")
+    }
+}
+
+secrets {
+    defaultPropertiesFileName = "secrets.defaults.properties"
 }
 
 android {
     namespace = "com.theblackbit.rickandmorty.core.network"
+}
+
+dependencies {
+    implementation(libs.apollo.runtime)
 }
